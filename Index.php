3083ccfb390cadac81e0c5af38267688f68b60e1
EduardoESP <eduardopaulosantana@gmail.com>
2022-04-30 20:11:57 -0300

    Add Index 1 e index2

---
+++ Index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>Meu primeiro site em PHP! Olá mundo</title>
+</head>
+	<body>
+		<?php
+			for($i= 0; $i <= 20 ; $i++){
+				print("Linha numero".$i ."<br/>");
+			}
+		
+		?>
+		
+		
+		
+	</body>
+</html>
